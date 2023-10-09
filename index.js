c7f1eb905aabe5ee709378e506ed9583b031e88b
Abdur-P-Rahim <bspirahim@gmail.com>
2023-10-09 15:21:24 +0600

    added single data by get method

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/jewellarys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await AdornamentsCollection.findOne(query)
+      res.send(result)
+    })
+
 
     app.post('/jewellarys', async(req, res)=>{
       const allJewellary = req.body;
